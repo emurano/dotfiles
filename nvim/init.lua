e298c86e6bf5252d046dbdb8e9da840528a65b99
Eric Murano <2396940+emurano@users.noreply.github.com>
2024-04-05 11:27:24 +1000

    Mved plugins to modules

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set encoding=UTF-8")
-vim.cmd("set smartindent")
-vim.cmd("inoremap jk <ESC>")
-vim.cmd("set backspace=indent,eol,start")
-vim.cmd("set number")
-vim.cmd("set noswapfile")
-vim.cmd("set relativenumber")
-vim.cmd("set hlsearch")
-vim.cmd("set ignorecase")
-vim.cmd("set incsearch")
-vim.cmd("set nowrap")
-vim.cmd("set scrolloff=8")
-vim.cmd("set cmdheight=1")
-vim.cmd("set exrc")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -31,44 +11,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  }
-}
-local opts = {}
-
--- Left it here - need to move plugins to external files and get them loading here
---require("lazy").setup("plugins")
-
-require("lazy").setup(plugins, opts)
-
-
-require("gruvbox").setup()
-vim.cmd.colorscheme "gruvbox"
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript", "typescript", "java"},
-  highlight = {enable = true},
-  indent = { enable = true }
-})
+require("vim-options")
+require("lazy").setup("plugins")
 
